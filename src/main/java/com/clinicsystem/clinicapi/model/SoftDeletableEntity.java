30ef4e5de0409cfbe9b6af101f23e0dfc6f857af
QuanT4_TanMai <davidnguyen67dev@gmail.com>
2026-02-28 10:33:34 +0700

    refactor: Update repositories to use UUID instead of Integer for entity identifiers refactor: Simplify soft delete logic in SoftDeletableEntity by removing isDeleted field

--- src/main/java/com/clinicsystem/clinicapi/model/SoftDeletableEntity.java
+++ src/main/java/com/clinicsystem/clinicapi/model/SoftDeletableEntity.java
@@ -22,31 +22,17 @@ public abstract class SoftDeletableEntity extends BaseEntity {
     @Column(name = "deleted_by", length = 100)
     private String deletedBy;
 
-    @Column(name = "is_deleted", nullable = false)
-    private Boolean isDeleted = false;
-
-    /**
-     * Soft delete this entity
-     */
     public void softDelete(String deletedBy) {
         this.deletedAt = LocalDateTime.now();
         this.deletedBy = deletedBy;
-        this.isDeleted = true;
     }
 
-    /**
-     * Restore soft deleted entity
-     */
     public void restore() {
         this.deletedAt = null;
         this.deletedBy = null;
-        this.isDeleted = false;
     }
 
-    /**
-     * Check if entity is soft deleted
-     */
     public boolean isDeleted() {
-        return Boolean.TRUE.equals(this.isDeleted);
+        return this.deletedAt != null;
     }
 }
